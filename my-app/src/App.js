325074c899260e194febae1cfb2700a08e902904
monikasingh95 <78414329+monikasingh95@users.noreply.github.com>
2022-03-11 10:21:51 +0530

    Logo is working

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,35 @@
+// import logo from './logo.svg';
 import './App.css';
+import logo from './airbnb-logo.png';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+//
+// export default App;
+
 
 function App() {
-  return (
-      <div>
-        <h1></h1>
-      </div>
-  );
+    return (
+        <img src={logo}/>
+    );
 }
 
 export default App;
