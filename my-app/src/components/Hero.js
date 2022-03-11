17351aaee170d7d66f6e26192fbba6ba75cb2e4a
monikasingh95 <78414329+monikasingh95@users.noreply.github.com>
2022-03-11 12:20:54 +0530

    added two more images to start second part of the project

--- my-app/src/components/Hero.js
+++ my-app/src/components/Hero.js
@@ -3,15 +3,16 @@ import picture from "./photo-grid.png";
 
 export default function Hero() {
     return(
-        <div>
-            <div className="hero-pic">
-                <img src={picture} />
+        <section>
+            <div className="hero--photo-container">
+                <img src={picture} className="hero-pic"/>
             </div>
             <div>
                 <h1 className="hero-heading">Online Experiences</h1>
                 <p className="hero-para">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
                 </p>
             </div>
-        </div>
+
+        </section>
     );
 }
