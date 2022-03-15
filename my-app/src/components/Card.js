b3b1050c18ee5df8892c1ff9868beed2298d409b
monikasingh95 <78414329+monikasingh95@users.noreply.github.com>
2022-03-15 17:35:19 +0530

    Passes props to card component but image is not showing

--- my-app/src/components/Card.js
+++ my-app/src/components/Card.js
@@ -2,18 +2,19 @@ import React from "react";
 import picture from "./katie-zaferes.png";
 import star from "./star.png";
 
-export default function Card () {
+export default function Card (props) {
+    console.log(props);
     return (
         <div className="card">
-            <img src={picture} className="card--pic"/>
+            <img src={`./${props.img}`}/>
             <div className="card--stats">
                 <img src={star} className="card--star"/>
-                <span> 5.0 </span>
+                <span> {props.rating} </span>
                 <span className="grey"> (6) . </span>
                 <span className="grey">USA</span>
             </div>
-            <p>Life Lessons with Katie Zaferes</p>
-            <p><strong>From $136</strong> / person</p>
+            <p>{props.title}</p>
+            <p><strong>From ${props.price}</strong> / person</p>
         </div>
     );
 }
