62e06eeb9e5b8f5dcf1d062fbf4d3dbc46d4730d
monikasingh95 <78414329+monikasingh95@users.noreply.github.com>
2022-03-23 01:05:32 +0530

    Added two more experiences

--- src/data.js
+++ src/data.js
@@ -37,5 +37,31 @@ export default [
         },
         location: "Norway",
         openSpots: 3,
+    },
+    {
+        id: 4,
+        title: "Experience Llamas",
+        description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
+        price: 136,
+        coverImg: "llama-hiking.jpg",
+        stats: {
+            rating: 5.0,
+            reviewCount: 6
+        },
+        location: "United States",
+        openSpots: 0,
+    },
+    {
+        id: 5,
+        title: "Learn the joy of cow milking",
+        description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+        price: 50,
+        coverImg: "milking-cow.jpg",
+        stats: {
+            rating: 4.8,
+            reviewCount: 2
+        },
+        location: "Norway",
+        openSpots: 3,
     }
 ]
